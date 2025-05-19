e0644a9c487d65141ae51ca83aa97a4d20b3d74a
alejandro <aortiz-2023a@corhuila.edu.co>
2025-05-18 20:16:54 -0500

    Add Ionic Angular project and starter components

---
+++ src/frontend/app-material-reserva/src/app/reservas/reservas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservas',
+  templateUrl: './reservas.component.html',
+  styleUrls: ['./reservas.component.scss'],
+})
+export class ReservasComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
